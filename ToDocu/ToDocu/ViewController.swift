7271fd8dc14933f8cd3ef573bf6eced06be53eaa
Vladislav Katsubo <vladislav.katsubo@gmail.com>
2023-07-13 08:48:29 +0300

    Initial configuration

---
+++ ToDocu/ToDocu/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ToDocu
+//
+//  Created by Vlad Katsubo on 13.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+}
